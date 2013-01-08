8d6556c267b095ebfb35cb83cdadba38649ce43f
strake <strake888@gmail.com>
2013-01-07 19:16:20 -0500

    derive Binary

--- Types.hs
+++ Types.hs
@@ -3,6 +3,7 @@ module Types where
 import Control.Applicative;
 import Control.Category.Unicode;
 import Data.Array;
+import Data.Binary;
 import Data.Function (on);
 import Data.List ((!!));
 import Data.Map as Map;
@@ -54,12 +55,12 @@ data Bar = Bar Clef Key (Map Rational (Set NR)) deriving (Show);
 
 data Piece = Piece (Array BarNumber Time) (Array (StaffNumber, BarNumber) Bar) deriving (Show);
 
-newtype BarNumber = BarNumber Int deriving (Eq, Ord, Ix, Show, Num, Enum, Real, Integral);
-newtype StaffNumber = StaffNumber Int deriving (Eq, Ord, Ix, Show, Num, Enum, Real, Integral);
+newtype BarNumber = BarNumber Int deriving (Eq, Ord, Ix, Show, Num, Enum, Real, Integral, Binary);
+newtype StaffNumber = StaffNumber Int deriving (Eq, Ord, Ix, Show, Num, Enum, Real, Integral, Binary);
 
 type Octave = Int;
 
-newtype PLtr = PLtr Char deriving (Eq, Ord);
+newtype PLtr = PLtr Char deriving (Eq, Ord, Binary);
 
 instance Show PLtr where {
   show (PLtr x) = [x];
@@ -77,3 +78,99 @@ keyPCs n =
     m = Map.fromListWith (+) $ (flip (,) (signum n) <$> take (abs n) gs) ++ (flip (,) 0 <$> take 7 gs);
   }
   in Set.fromList $ uncurry PitchClass <$> Map.assocs m;
+
+{-!
+deriving instance Binary PitchClass
+deriving instance Binary Pitch
+deriving instance Binary Length
+deriving instance Binary Time
+deriving instance Binary NR
+deriving instance Binary Clef
+deriving instance Binary Bar
+deriving instance Binary Piece
+!-}
+
+-- GENERATED START
+
+ 
+instance Binary PitchClass where
+        put (PitchClass x1 x2)
+          = do put x1
+               put x2
+        get
+          = do x1 <- get
+               x2 <- get
+               return (PitchClass x1 x2)
+
+ 
+instance Binary Pitch where
+        put (Pitch x1 x2)
+          = do put x1
+               put x2
+        get
+          = do x1 <- get
+               x2 <- get
+               return (Pitch x1 x2)
+
+ 
+instance Binary Length where
+        put (Length x1 x2)
+          = do put x1
+               put x2
+        get
+          = do x1 <- get
+               x2 <- get
+               return (Length x1 x2)
+
+ 
+instance Binary Time where
+        put (Time x1 x2)
+          = do put x1
+               put x2
+        get
+          = do x1 <- get
+               x2 <- get
+               return (Time x1 x2)
+
+ 
+instance Binary NR where
+        put (NR x1 x2)
+          = do put x1
+               put x2
+        get
+          = do x1 <- get
+               x2 <- get
+               return (NR x1 x2)
+
+ 
+instance Binary Clef where
+        put (Clef x1 x2)
+          = do put x1
+               put x2
+        get
+          = do x1 <- get
+               x2 <- get
+               return (Clef x1 x2)
+
+ 
+instance Binary Bar where
+        put (Bar x1 x2 x3)
+          = do put x1
+               put x2
+               put x3
+        get
+          = do x1 <- get
+               x2 <- get
+               x3 <- get
+               return (Bar x1 x2 x3)
+
+ 
+instance Binary Piece where
+        put (Piece x1 x2)
+          = do put x1
+               put x2
+        get
+          = do x1 <- get
+               x2 <- get
+               return (Piece x1 x2)
+-- GENERATED STOP
