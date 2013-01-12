75b71e5a236c6b27677c25a946c3316d4ef3099a
strake <strake888@gmail.com>
2013-01-12 09:04:54 -0500

    hint

--- mused.hs
+++ mused.hs
@@ -60,8 +60,8 @@ st_view (St { st_w = (P p, P q) }) = view (P p) (q-p);
 
 main = do {
   vp <- newIORef (Piece (array (1, 1) [(1, Time 1 0)]) (array ((1, 1), (1, 1)) [((1, 1), Bar (Clef (PLtr 'B') 4) 0 Map.empty)]));
-  φp <- newIORef Nothing; -- phantom note
   stp <- newIORef (St { st_l = Length 0 0, st_a = 0, st_time = Time 1 0, st_key = 0, st_clef = Clef (PLtr 'B') 4, st_w = (p2 (-40, -37), p2 (0, 3)) });
+  hintp <- newIORef Nothing; -- hint note
   filename_p <- newIORef "";
 
   GTK.initGUI;
